bfdc9a9440d61364d93fbb4a9ef04fd53209a392
renriquez2019 <92184586+renriquez2019@users.noreply.github.com>
2022-08-19 14:52:19 -0400

    changed logo

--- frontend/src/pages/SignIn.js
+++ frontend/src/pages/SignIn.js
@@ -8,6 +8,7 @@ import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Divider, dividerClasses } from "@mui/material";
 
 
 const theme = createTheme();
@@ -22,60 +23,8 @@ export default function SignIn() {
 
 
     return (
-       <ThemeProvider theme = {theme}>
-            <Container component ="main" maxWidth ="xs">
-                <CssBaseline />
-                <Box
-                    sx={{
-                        marginTop: 25,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    <Typography variant="h4">
-                        Sign In
-                    </Typography>
-
-                    <Box component="form" sx={{mt: 3}}>
-                        <Grid container spacing={2}>
-
-                            <Grid item xs={12}>
-                                <TextField
-                                    fullWidth
-                                    variant ="outlined"
-                                    type = "email"
-                                    id ="email"
-                                    name ="email"
-                                    label="Enter your email:"
-                                    onChange={(e) => setEmail(e.target.value)}
-                                />
-                            </Grid>
-
-                            <Grid item xs={12}>
-                                <TextField
-                                    fullWidth
-                                    variant ="outlined"
-                                    type ="password"
-                                    id ="password"
-                                    name="password"
-                                    label="Enter your password:"
-                                    onChange={(e) => setPassword(e.target.value)}
-                                />
-                            </Grid>
-
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                sx={{mt: 3, mb: 2}}
-                            >
-                                Login 
-                            </Button>
-                        </Grid>
-                    </Box>
-                </Box>
-            </Container>
-       </ThemeProvider>       
+       <div>
+        
+       </div>     
     );
 }
