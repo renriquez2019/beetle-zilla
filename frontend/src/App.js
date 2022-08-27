8b4aee50366e3ffdc5e54727bd651aea8eae1bb1
renriquez2019 <92184586+renriquez2019@users.noreply.github.com>
2022-08-27 18:49:00 -0400

    changed to bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
 import {Route, Routes, BrowserRouter} from "react-router-dom";
 
 import SignIn from './pages/SignIn';
--- frontend/src/pages/SignIn.js
+++ frontend/src/pages/SignIn.js
@@ -1,15 +1,9 @@
 import React, { useState, useEffect } from "react";
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
-import CssBaseline  from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
+
+
+import {Button, Alert, Form} from 'react-bootstrap'
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Divider, dividerClasses } from "@mui/material";
-import Login from "../components/Login";
+
 
 
 const theme = createTheme();
@@ -24,8 +18,40 @@ export default function SignIn() {
 
 
     return (
-       <div className= "signin">
-            <Login></Login>
-       </div>     
+        <div className="Auth-form-container">
+            <form className="Auth-form">
+                <div className="Auth-form-content">
+                    <h3 className="Auth-form-title">Sign In</h3>
+                    <div className="text-center">
+                        Not registered yet?{" "}
+                        <a href="#"> Sign Up</a>
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>Email address</label>
+                        <input
+                            type="email"
+                            className="form-control mt-1"
+                            placeholder="Enter email"
+                        />
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            className="form-control mt-1"
+                            placeholder="Enter password"
+                        />
+                    </div>
+                    <div className="d-grid gap-2 mt-3">
+                        <button type="submit" className="btn btn-primary">
+                            Submit
+                        </button>
+                    </div>
+                    <p className="forgot-password text-center mt-2">
+                        Forgot <a href="#">password?</a>
+                    </p>
+                </div>  
+            </form> 
+        </div> 
     );
 }
