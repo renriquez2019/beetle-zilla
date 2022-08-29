31b8bfcd7602bc476ac14bf3ecb5c72d96d94ebe
renriquez2019 <92184586+renriquez2019@users.noreply.github.com>
2022-08-28 20:48:48 -0400

    commit

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,15 @@
+import logo from '../img/bug.png'
+
 export default function Header() {
 
     return (
-        <div>
-            
+        <div className="header-container">
+            <div className="logo-container">
+                <a href="/dashboard" className="title-text">
+                    <img src={logo} height="58px" alt="Logo" />
+                    Bug Tracker
+                </a>
+            </div>
         </div>
     );
 }
--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,9 +1,9 @@
-
+import Header from "../components/Header";
 
 export default function Dashboard() {
     return (
         <div className="dashboard">
-
+            <Header></Header>
         </div>
     );
 }
