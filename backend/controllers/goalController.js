ff5c1fbd8a5d6421cf4ea0eb28feb6b2568f4f12
renriquez2019 <92184586+renriquez2019@users.noreply.github.com>
2022-08-30 18:03:15 -0400

    initial commit for rest api

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,37 +1,62 @@
-const asyncHandler = require('express-async-handler')
+
+const Goal = require('../models/goalModel')
 
 // @desc Get goals
 // @route GET /api/goals
 // access Private
-const getGoals = asyncHandler ((req, res) => {
-    res.status(200).json({message: 'Get goals'})
-})
+const getGoals = async (req, res) => {
+    const goals = await Goal.find()
+    
+    res.status(200).json(goals)
+}
 
 // @desc Set goals
 // @route POST /api/goals
 // access Private
-const setGoal = asyncHandler ((req, res) => {
+const setGoal = async (req, res) => {
     if (!req.body.text) {
         res.status(400)
         throw new Error('Please add a text field')
     }
 
-    res.status(200).json({message: 'Set goal'})
-})
+    const goal = await Goal.create({
+        text: req.body.text,
+    })
+
+    res.status(200).json(goal)
+}
 
 // @desc Update goals
 // @route PUT /api/goal/:id
 // access Private
-const updateGoal = asyncHandler ((req, res) => {
-    res.status(200).json({message: `Update goal ${req.params.id}`})
-})
+const updateGoal = async (req, res) => {
+    const goal = await Goal.findById(req.params.id)
+
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true,})
+
+    res.status(200).json(updatedGoal)
+}
 
 // @desc Delete goals
 // @route DELETE /api/goal/:id
 // access Private
-const deleteGoal = asyncHandler ((req, res) => {
-    res.status(200).json({message: `Delete goal ${req.params.id}`})
-})
+const deleteGoal = async (req, res) => {
+    const goal = await Goal.findById(req.params.id)
+
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    await goal.remove()
+
+    res.status(200).json({id: req.params.id})
+}
 
 
 module.exports = {
