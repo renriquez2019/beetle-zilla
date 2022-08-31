3d4a6e44dda43c7a0283ab2609a85f3b14e37552
renriquez2019 <92184586+renriquez2019@users.noreply.github.com>
2022-08-30 20:44:20 -0400

    backend update

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,6 +5,7 @@ const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGO_URI)
 
         console.log(`MongoDB Connected: ${conn.connection.host}` .cyan.underline);
+        
     } catch (error) {
         console.log(error);
         process.exit(1);
