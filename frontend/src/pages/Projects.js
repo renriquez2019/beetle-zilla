b696fdbb3219b40e9ec2e85e48ecad975e03b818
renriquez2019 <92184586+renriquez2019@users.noreply.github.com>
2022-09-02 05:09:25 -0400

    added Table to projects

--- frontend/src/pages/Projects.js
+++ frontend/src/pages/Projects.js
@@ -7,20 +7,24 @@ import {
     TableBody, 
     TableRow, 
     TableCell,
-    TableContainer,  
+    TableContainer,
+    TablePagination,  
     Paper,
-    createTheme
+    Button
 } from "@mui/material"
 
-import {fontSize, height, styled} from '@mui/system'
-
-import {Button} from '@mui/material';
+import {BsFillDashSquareFill, BsFillTrashFill} from 'react-icons/bs'
+import { styled } from '@mui/system'
 
 
 export default function Projects() {
 
     const [sidebar, setSidebar] = useState(true)
     const toggleSidebar = () => setSidebar(!sidebar)
+    const [selected, setSelected] = useState([]);
+    const [page, setPage] = useState(0);
+    const [dense, setDense] = useState(false);
+    const [rowsPerPage, setRowsPerPage] = useState(5);
 
 
     function createData(name, desc, numtickets, numusers) {
@@ -36,7 +40,25 @@ export default function Projects() {
         createData('Demo Project 2', "This is project 2", 1, 0),
         createData('Demo Project 2', "This is project 2", 1, 0),
         createData('Demo Project 2', "This is project 2", 1, 0),
+        createData('Demo Project 2', "This is project 2", 1, 0),
     ];
+
+    function descendingComparator(a, b, orderBy) {
+        if (b[orderBy] < a[orderBy]) {
+          return -1;
+        }
+        if (b[orderBy] > a[orderBy]) {
+          return 1;
+        }
+        return 0;
+    }
+      
+    function getComparator(order, orderBy) {
+        return order === 'desc'
+          ? (a, b) => descendingComparator(a, b, orderBy)
+          : (a, b) => -descendingComparator(a, b, orderBy);
+    }
+      
 
     const HeaderTableCell = styled(TableCell)({
         color: '#ffff',
@@ -55,6 +77,19 @@ export default function Projects() {
         borderColor: 'black'
     })
 
+    const StyledButton = styled(Button)({
+        borderRadius: 10,
+    })
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+    
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
     return (
         <div>
             <Header openSidebar={toggleSidebar} />
@@ -64,6 +99,15 @@ export default function Projects() {
                 <div className="pagetitle">
                     <h1>Projects</h1>
                 </div>
+
+                <div className="btn-add">
+                    <StyledButton
+                        variant="contained"
+                        color="success"
+                        size="medium">
+                        Create new Project
+                    </StyledButton>
+                </div>
                 
                 <TableContainer 
                     component={Paper} 
@@ -72,22 +116,23 @@ export default function Projects() {
                         borderRadius: 10,
                         margin: '10px 10px',
                     }}>
-                    <Table sx={{ minWidth: 650, borderCollapse: 'separate', }}  aria-label="simple table">
+                    <Table sx={{}}  aria-label="simple table">
                         <TableHead
                             sx = {{
                             backgroundColor: '#012970',
-                            color: '#ffff'
-                        }}>
+                            color: '#ffff',
+                            borderWidth: '2px',
+                            borderColor: 'black'
+                            }}>
                             <TableRow>
                                 <HeaderTableCell>Project Name</HeaderTableCell>
                                 <HeaderTableCell sx = {{paddingRight: 90}}>Description</HeaderTableCell>
                                 <HeaderTableCell align = "center"># of Tickets</HeaderTableCell>
                                 <HeaderTableCell align = "center"># of Users</HeaderTableCell>
-                                <HeaderTableCell align = "center" sx = {{paddingRight: 5}}>Actions</HeaderTableCell> 
+                                <HeaderTableCell align = "center">Actions</HeaderTableCell> 
                             </TableRow>
                         </TableHead>
                         <TableBody>
-
                             {rows.map((row) => (
                                 <TableRow
                                     key={row.name}
@@ -96,16 +141,26 @@ export default function Projects() {
                                     <StyledTableCell>{row.desc}</StyledTableCell>
                                     <StyledTableCell align = "center">{row.numtickets}</StyledTableCell>
                                     <StyledTableCell align = "center">{row.numusers}</StyledTableCell>
+                                    <StyledTableCell>
+                                        <div className="actions-icon">
+                                            <BsFillDashSquareFill className="details"/>
+                                            <BsFillTrashFill className="trash"/>
+                                        </div>
+                                    </StyledTableCell>
                                 </TableRow>
                             ))}
-
                         </TableBody>
                     </Table>
                 </TableContainer>
-                
-                <Button>
-                    Add new Project
-                </Button>
+                <TablePagination
+                    rowsPerPageOptions={[5, 10, 25]}
+                    
+                    count={rows.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                />
             </div>
         </div>
     );
