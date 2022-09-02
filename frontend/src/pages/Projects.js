ab60eb482480abc51e5dff8154df3f177e823564
renriquez2019 <92184586+renriquez2019@users.noreply.github.com>
2022-09-02 01:49:53 -0400

    made Table

--- frontend/src/pages/Projects.js
+++ frontend/src/pages/Projects.js
@@ -1,12 +1,27 @@
 import { useState } from "react";
 import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
-import { TableContainer, Table, TableHead, TableBody, TableRow, TableCell, Paper } from "@mui/material"
+import { 
+    Table, 
+    TableHead, 
+    TableBody, 
+    TableRow, 
+    TableCell,
+    TableContainer,  
+    Paper,
+    createTheme
+} from "@mui/material"
+
+import {fontSize, height, styled} from '@mui/system'
+
+import {Button} from '@mui/material';
+
 
 export default function Projects() {
 
     const [sidebar, setSidebar] = useState(true)
     const toggleSidebar = () => setSidebar(!sidebar)
+
 
     function createData(name, desc, numtickets, numusers) {
         return { name, desc, numtickets, numusers};
@@ -21,8 +36,24 @@ export default function Projects() {
         createData('Demo Project 2', "This is project 2", 1, 0),
         createData('Demo Project 2', "This is project 2", 1, 0),
         createData('Demo Project 2', "This is project 2", 1, 0),
-
     ];
+
+    const HeaderTableCell = styled(TableCell)({
+        color: '#ffff',
+        height: '50px',
+        fontWeight: 'bolder',
+        fontSize: '18px',
+        fontFamily: 'Nunito',
+    })
+
+    const StyledTableCell = styled(TableCell)({
+        color: '#012970',
+        fontWeight: 'bold',
+        fontSize: '14px',
+        fontFamily: 'Nunito',
+        borderWidth: '2px',
+        borderColor: 'black'
+    })
 
     return (
         <div>
@@ -34,14 +65,25 @@ export default function Projects() {
                     <h1>Projects</h1>
                 </div>
                 
-                <TableContainer component={Paper} sx ={{backgroundColor: '#ffff'}}>
-                    <Table sx={{ minWidth: 650}}  aria-label="simple table">
-                        <TableHead className="table-head">
+                <TableContainer 
+                    component={Paper} 
+                    sx ={{
+                        backgroundColor: '#ffff',
+                        borderRadius: 10,
+                        margin: '10px 10px',
+                    }}>
+                    <Table sx={{ minWidth: 650, borderCollapse: 'separate', }}  aria-label="simple table">
+                        <TableHead
+                            sx = {{
+                            backgroundColor: '#012970',
+                            color: '#ffff'
+                        }}>
                             <TableRow>
-                                <TableCell>Project Name</TableCell>
-                                <TableCell>Description</TableCell>
-                                <TableCell># of Tickets</TableCell>
-                                <TableCell># of Users</TableCell> 
+                                <HeaderTableCell>Project Name</HeaderTableCell>
+                                <HeaderTableCell sx = {{paddingRight: 90}}>Description</HeaderTableCell>
+                                <HeaderTableCell align = "center"># of Tickets</HeaderTableCell>
+                                <HeaderTableCell align = "center"># of Users</HeaderTableCell>
+                                <HeaderTableCell align = "center" sx = {{paddingRight: 5}}>Actions</HeaderTableCell> 
                             </TableRow>
                         </TableHead>
                         <TableBody>
@@ -49,19 +91,21 @@ export default function Projects() {
                             {rows.map((row) => (
                                 <TableRow
                                     key={row.name}
-                                    sx ={{ '&:last-child td, &:last-child th': {border: 0}}} 
-                                >
-                                    <TableCell component="th" scope="row">{row.name}</TableCell>
-                                    <TableCell>{row.desc}</TableCell>
-                                    <TableCell>{row.numtickets}</TableCell>
-                                    <TableCell>{row.numusers}</TableCell>
+                                    >
+                                    <StyledTableCell component="th" scope="row">{row.name}</StyledTableCell>
+                                    <StyledTableCell>{row.desc}</StyledTableCell>
+                                    <StyledTableCell align = "center">{row.numtickets}</StyledTableCell>
+                                    <StyledTableCell align = "center">{row.numusers}</StyledTableCell>
                                 </TableRow>
                             ))}
 
                         </TableBody>
                     </Table>
                 </TableContainer>
-
+                
+                <Button>
+                    Add new Project
+                </Button>
             </div>
         </div>
     );
