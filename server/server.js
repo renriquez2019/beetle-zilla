bfdd6bbc203cf1ec40416a2bea22664773a82617
renriquez2019 <92184586+renriquez2019@users.noreply.github.com>
2022-10-10 01:08:37 -0400

    prepare for deployment

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const cors = require('cors')
 const {errorHandler} = require('./middleware/errorMiddleware')
@@ -18,6 +19,17 @@ app.use(
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/projects', require('./routes/projectRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
+
+// Serve frontend
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html')))
+
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'))
+}
 
 
 const port = process.env.PORT
